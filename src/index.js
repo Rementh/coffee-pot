95413915e1613c14858a8f41a8acafbe27df3b57
Filip Talaga <talaga.filip@gmail.com>
2019-02-08 20:31:10 +0100

    Added offline caching, and install prompt into service-worker

--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
 module.hot.accept();
-
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/service-worker.js').then(registration => {
-            console.log('SW registered: ', registration);
-        }).catch(registrationError => {
-            console.log('SW registration failed: ', registrationError);
-        });
-    });
-}
+serviceWorker.register();
--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,8 @@ module.exports = {
             // these options encourage the ServiceWorkers to get in there fast
             // and not allow any straggling "old" SWs to hang around
             clientsClaim: true,
-            skipWaiting: true
+            skipWaiting: true,
+            navigateFallback: '/index.html'
         })
     ],
     devServer: {
